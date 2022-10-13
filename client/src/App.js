1eaa148341d3bfc1593c02dd9bb62bd4eb707c02
Kevin Vazquez <kevin123vazquezava@gmail.com>
2022-10-13 17:39:22 -0300

    Here i updated some part of the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
 import { useState } from "react";
 import Register from "./components/register";
 import "./index.css";
+import Navigation from "./components/navigation";
 
 function App() {
   const [pressed, setPressed] = useState(false);
 
   return (
     <div className="container">
+      <Navigation/>
       {
         !pressed && 
         <div className="presentation">
-          <nav className="presentation-nav">
-            <img src="https://play-lh.googleusercontent.com/EnxJKkoXzxrmA0RA0gUie0K4gvbRLrQhpflyMzwSMvCfk2FA6o9sBLzfWtG5qLzIBIc"/>
-            <h3>Kevin Vazquez</h3>
-            <ul>
-              <li>HOME</li>
-              <li>PROJECTS</li>
-              <li>CONTACT</li>
-            </ul>
-          </nav>
+          disculpa
+          
         </div>
       }
       {
--- client/src/components/navigation.js
+++ client/src/components/navigation.js
@@ -1,15 +1,14 @@
+import { StrictMode } from "react";
 import { MdOutlineMessage } from "react-icons/md";
 import { BsGithub, BsInstagram } from "react-icons/bs"
 
 export default function Navigation() {
     return (
-        <nav className="nav-main">
-            <i><MdOutlineMessage/></i>
-            <h1>Chatting is Fun</h1>
-            <span className="social-section">
-                <a href="https://github.com/Kevin-Vazquez" target="_blank"><i><BsGithub/></i></a>
-                <a href="https://www.instagram.com/kevin_vazquez_martinez/?next=%2F" target="_blank"><i><BsInstagram/></i></a>
-            </span>
-        </nav>
+        <StrictMode>
+        <nav className="presentation-nav">
+            <img src="https://play-lh.googleusercontent.com/EnxJKkoXzxrmA0RA0gUie0K4gvbRLrQhpflyMzwSMvCfk2FA6o9sBLzfWtG5qLzIBIc"/>
+            <h3>Kevin Vazquez</h3>
+          </nav>
+        </StrictMode>
     );
 }
