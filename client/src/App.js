258bb1c6cd0114ed351942b1e8c351b536cfbd01
Kevin Vazquez <kevin123vazquezava@gmail.com>
2022-10-24 16:35:26 -0300

    Aqui agregue informacion mia acerca que es lo que hago

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import "./index.css";
 import Navigation from "./components/navigation";
+import Info from "./components/info";
 import { BsChevronDown } from "react-icons/bs";
 
 function App() {
+  const click = ()=>{
 
+  }
   return (
     <div className="container">
       <Navigation/>
         <div className="presentation">
           <div className="presentation-header">
             <h1>Hi! I am Kevin Vazquez and i build web applications.</h1>
-            <div className="info-to-scroll-container">
+            <div className="info-to-scroll-container" onClick={click}>
               <p className="info-to-scrollDown-p">Scroll down to learn more about me.</p>
               <p className="info-to-scrollDown-icon"><BsChevronDown/></p>
             </div>
           </div>
         </div>
+        <Info/>
     </div>
   );
 }
---
+++ client/src/components/info.js
@@ -0,0 +1,12 @@
+import "../components/App.css";
+
+export default function Info() {
+    return (
+            <div className="info-container">
+            <p>We are in the age of technology, in which technology is a very big help to grow companies. It became essential for today's business. 
+                <br/>
+                <br/>
+                Well, my name is Kevin Damian Vazquez Martinez and I'm a web developer. A person who creates both web pages and web applications. I am a developer who develops both Front-End and Back-End</p>
+            </div>
+    );
+}
